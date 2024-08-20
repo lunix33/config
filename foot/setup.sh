626e54e90e0ddd2e1dc43c424ce7e1d789d9bb5b
Patrick Bergeron <pbergeron92@gmail.com>
2024-08-19 23:55:10 -0400

    Include custom foot terminal setup

---
+++ foot/setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+HERE="$(cd $(dirname $0); pwd)"
+DESTINATION="${XDG_CONFIG_HOME:-${HOME}/.config}/foot"
+
+set -euo pipefail
+
+echo "i> Ensure 'foot' is installed..."
+case "$(lsb_release --id --short)" in
+	"Arch")
+		sudo pacman --sync --needed --noconfirm foot
+		;;
+	*)
+		echo "Unsupported OS..."
+		echo "Do what you need to, to install: foot"
+		;;
+esac
+
+if [ -e "${DESTINATION}" ]; then
+	echo "!> ${DESTINATION} already exists, removing old content."
+	rm -fr ${DESTINATION}
+fi
+echo "i> Linking configuration..."
+ln --symbolic --verbose ${HERE} ${DESTINATION}
+
