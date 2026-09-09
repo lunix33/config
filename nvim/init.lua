b84cc4c9e23e60bfc5112cb31da7bb6c9919d9f3
Patrick Bergeron <pbergeron@biointelligence.com>
2026-09-09 15:07:29 -0400

    Ensure LSP settings and set rust-analyzer to use clippy and all features

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,5 +3,6 @@ require("settings")
 require("keymaps.general")
 require("keymaps.lsp")
 require("plugins")
+require("lsp_settings")
 
 vim.cmd.colorscheme("onelight")
--- nvim/lua/lsp_settings.lua
+++ nvim/lua/lsp_settings.lua
@@ -1,3 +1,2 @@
-return {
-	lua_ls = require("lsp_settings.lua_ls"),
-}
+vim.lsp.config("lua_ls", require("lsp_settings.lua_ls"))
+vim.lsp.config("rust_analyzer", require("lsp_settings.rust_analyzer"))
